945fb1356a1ae0651e2e9d82854aa3838ff9e409
gmslc <919226548@qq.com>
2024-09-05 18:36:57 +0800

    fix: bug for func convertDnsRecord and splitSuffixByLongestMatch

--- internal/ddi/provider.go
+++ internal/ddi/provider.go
@@ -134,24 +134,26 @@ func (p *Provider) convertDnsRecord(req []*endpoint.Endpoint) (map[string][]*DNS
 			rd[suff] = make([]*DNSRecord, 0)
 		}
 
-		dnsr := &DNSRecord{
-			Name:        pre,
-			Rtype:       ep.RecordType,
-			TTL:         uint32(ep.RecordTTL),
-			TTLStrategy: strategyRewrite,
-			Rdata:       ep.Targets[0],
+		for _, target := range ep.Targets {
+			dnsr := &DNSRecord{
+				Name:        pre,
+				Rtype:       ep.RecordType,
+				TTL:         uint32(ep.RecordTTL),
+				TTLStrategy: strategyRewrite,
+				Rdata:       target,
 
-			Enabled: true,
-			Source:  source,
+				Enabled: true,
+				Source:  source,
+			}
+			if p.client.Config.DefaultTTL == 0 && ep.RecordTTL == 0 {
+				dnsr.TTLStrategy = strategyInherit
+			}
+			if p.client.Config.DefaultTTL != 0 && ep.RecordTTL == 0 {
+				// if the TTL is not set and the default TTL is not 0, use the default TTL
+				dnsr.TTL = p.client.Config.DefaultTTL
+			}
+			rd[suff] = append(rd[suff], dnsr)
 		}
-		if p.client.Config.DefaultTTL == 0 && ep.RecordTTL == 0 {
-			dnsr.TTLStrategy = strategyInherit
-		}
-		if p.client.Config.DefaultTTL != 0 && ep.RecordTTL == 0 {
-			// if the TTL is not set and the default TTL is not 0, use the default TTL
-			dnsr.TTL = p.client.Config.DefaultTTL
-		}
-		rd[suff] = append(rd[suff], dnsr)
 	}
 
 	return rd, nil
